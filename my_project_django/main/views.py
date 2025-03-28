8b9abf39d613ab6a6c9eade6539ab9a7e6727278
Azatikone <azatikone@gmail.com>
2025-03-29 01:52:07 +0400

    update django app main

--- my_project_django/main/views.py
+++ my_project_django/main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index():
+def index(request):
     return HttpResponse("<h4>Проверка работы</h4>")
+
+def about(request):
+    return HttpResponse("<h4>Страница про нас</h4>")
+
+def sosal(request):
+    return HttpResponse("<h4>СОСАЛ??</h4>")
